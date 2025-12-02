4aa4a97b0af8e208b6f913033a67daaa2ef1015a
Abayomi Olagunju <olagunjujeremiah@gmail.com>
2025-12-02 17:53:13 +0000

    added adsense code

--- UI/frontend/app/layout.tsx
+++ UI/frontend/app/layout.tsx
@@ -54,6 +54,8 @@ export default function RootLayout({
   return (
         <html lang="en" suppressHydrationWarning className="dark" style={{ colorScheme: 'dark' }}>
           <head>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7495909984574657"
+     crossorigin="anonymous"></script>
             <link rel="icon" href="/fav.png" />
             <meta
               name="viewport"
